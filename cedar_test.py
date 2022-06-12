99c702921f17268f099d1a8392ecace0ecca84d5
Phil Schumm <pschumm@uchicago.edu>
2022-06-12 16:05:37 -0500

    Update testing script to handle radio fields and verify resulting metadata following entry

--- cedar_test.py
+++ cedar_test.py
@@ -8,23 +8,81 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import keyring
 import json
+import random
+import requests
+import logging
+import time
 
-url = 'https://cedar.metadatacenter.org/'
-user = 'pschumm@uchicago.edu'
-template = '195559f9-9385-49ac-8c3c-f46e939f36a3row'
-wait = 5
+URL = 'https://cedar.metadatacenter.org/'
+API_BASE = 'https://resource.metadatacenter.org/'
+USER = 'pschumm@uchicago.edu'
+TEMPLATE = 'e221c223-2400-424b-a603-bbc6e6de14a7row'
 
-def enter_text(label, value):
-    WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.XPATH, f'//div[contains(text(), "{label}")]/../..//div[@class="answer ng-scope"]')))
-    field = driver.find_element(By.XPATH, f'//div[contains(text(), "{label}")]/../..//div[@class="answer ng-scope"]')
+# List of words for constructing random entries
+with open('/usr/share/dict/words') as f:
+    lexicon = [line.strip() for line in f]
+
+handler = logging.FileHandler('cedar-testing.log')
+handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
+logger = logging.getLogger('cedar-testing')
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+form = [('text', ('Minimal Info', 'study_name'), 'Study Title or Name', None, 5),
+        ('text', ('Minimal Info', 'study_description'), 'Study Description or Abstract', None, 24),
+        ('text', ('Minimal Info', 'study_nickname'), 'Study Nickname or Alternative Title', None, 3),
+        ('text', ('Metadata Location Updated', 'Metadata Location - Details', 'nih_application_id'), 'NIH Application ID', None, 1),
+        ('text', ('Metadata Location Updated', 'Metadata Location - Details', 'nih_reporter_link'), 'NIH RePORTER Link', 'http://example.com'),
+        ('text', ('Metadata Location Updated', 'Metadata Location - Details', 'clinical_trials_study_id'), 'ClinicalTrials.gov Study ID', None, 1),
+        ('text', ('Metadata Location Updated', 'Data Repositories', 'repository_name'), 'Name of Repository', None, 3),
+        ('text', ('Metadata Location Updated', 'Data Repositories', 'repository_study_ID'), 'Study ID assigned by Repository', None, 1),
+        ('text', ('Metadata Location Updated', 'Data Repositories', 'repository_persistent_ID'), 'Repository-branded Study Persistent Identifier', None, 1),
+        ('text', ('Metadata Location Updated', 'Data Repositories', 'repository_citation'), 'Study citation at Repository', None, 12),
+        ('text', ('Metadata Location Updated', 'cedar_study_level_metadata_template_instance_ID'), 'CEDAR Study-level Metadata Template Instance ID', None, 12),
+        ('text', ('Metadata Location Updated', 'other_study_websites'), 'Other Study-Associated Websites', 'http://example.com'),
+        ('radio', ('Citation', 'heal_funded_status'), 'Is this study HEAL-funded?')]
+
+# Increase in case of TimeoutException
+WAIT = 5
+
+def enter_text(values, fieldname, label, value=None, words=1):
+    if value is None:
+        value = ' '.join(random.sample(lexicon, words))
+    field = WebDriverWait(driver, WAIT).until(
+        EC.presence_of_element_located((By.XPATH, f'//div[contains(text(), "{label}")]/../..//div[@class="answer ng-scope"]'))
+    )
     field.click()
-    field = driver.find_element(By.XPATH, f'//div[contains(text(), "{label}")]/../..//input')
+    field = WebDriverWait(driver, WAIT).until(
+        EC.presence_of_element_located((By.XPATH, f'//div[contains(text(), "{label}")]/../..//input'))
+    )
     field.send_keys(value)
-    driver.find_element(By.XPATH, f'//div[contains(text(), "{label}")]/../..//button[@type="submit"]').click()
+    values[fieldname] = value
+    submit = WebDriverWait(driver, WAIT).until(
+        EC.presence_of_element_located((By.XPATH, f'//div[contains(text(), "{label}")]/../..//button[@type="submit"]'))
+    )
+    time.sleep(1)
+    submit.click()
+
+def enter_radio(values, fieldname, label):
+    field = WebDriverWait(driver, WAIT).until(
+        EC.presence_of_element_located((By.XPATH, f'//div[contains(text(), "{label}")]/../..//div[@class="answer ng-scope"]'))
+    )
+    field.click()
+    time.sleep(1)
+    options = driver.find_elements(By.XPATH, f'//div[contains(text(), "{label}")]//../..//input[@type="radio"]')
+    option = random.choice(options)
+    option.click()
+    values[fieldname] = option.accessible_name
+    submit = WebDriverWait(driver, WAIT).until(
+        EC.presence_of_element_located((By.XPATH, f'//div[contains(text(), "{label}")]/../..//button[@type="submit"]'))
+    )
+    submit.click()
 
 def wait_for_uuid():
     def has_uuid(driver):
-        element = driver.find_element(By.XPATH, '//pre[@id="form-json-preview"]')
+        element = WebDriverWait(driver, WAIT).until(
+            EC.presence_of_element_located((By.XPATH, '//pre[@id="form-json-preview"]'))
+        )
         data = json.loads(element.get_attribute('textContent'))
         if '@id' in data:
             return data['@id'].rsplit('/', 1)[1]
@@ -32,71 +90,77 @@ def wait_for_uuid():
             return False
     return has_uuid
 
+def populate_template(TEMPLATE, form):
+    WebDriverWait(driver, WAIT).until(EC.presence_of_element_located((By.ID, TEMPLATE)))
+    driver.find_element(By.XPATH, f'//div[@id="{TEMPLATE}"]//button').click()
+    
+    values = {}
+    for field in form:
+        if field[0]=='text':
+            enter_text(values, field[1][-1], *field[2:])
+        elif field[0]=='radio':
+            enter_radio(values, field[1][-1], *field[2:])
+    
+    # Save and return to workspace
+    driver.find_element(By.XPATH, '//button[text()="Save"]').click()
+    uuid = WebDriverWait(driver, WAIT).until(wait_for_uuid())
+    WebDriverWait(driver, WAIT).until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Created")]')))
+    driver.find_element(By.XPATH, '//i[contains(@class, "back-arrow")]').click()
+    logger.info(f'Created metadata instance: {uuid}')
+    
+    return uuid, values
+
+def verify_metadata(uuid, values):
+    endpoint='template-instances'
+    key = keyring.get_password('resource.metadatacenter.org', USER)
+    instance = f'https%3A%2F%2Frepo.metadatacenter.org%2F{endpoint}%2F{uuid}'
+    url = f'{API_BASE}{endpoint}/{instance}?format=json'
+    response = requests.get(url, headers={'Accept':'application/json',
+                                          'Authorization':f'apiKey {key}'})
+    metadata = response.json()
+    
+    valid = invalid = 0
+    for field in form:
+        fieldname = field[1][-1]
+        value = metadata
+        for key in field[1]:
+            try:
+                value = value[key]
+            except TypeError:
+                value = value[0][key]
+        if value == values[fieldname]:
+            valid += 1
+        else:
+            logger.info(f'Value "{value}" invalid for {field[1][-1]}: "{values[fieldname]}"')
+            invalid += 1
+    logger.info(f'{valid} fields valid; {invalid} invalid')
+
+def delete_instance(uuid):
+    endpoint='template-instances'
+    key = keyring.get_password('resource.metadatacenter.org', 'pschumm@uchicago.edu')
+    instance = f'https%3A%2F%2Frepo.metadatacenter.org%2Ftemplate-instances%2F{uuid}'
+    url = f'{API_BASE}{endpoint}/{instance}'
+    response = requests.delete(url, headers={'Authorization':f'apiKey {key}'})
+    logger.info(f'Metadata instance {uuid} deleted')
+
+
 # Instantiate driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(url)
+driver.get(URL)
 
 # Log in
-WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.ID, 'kc-login')))
+WebDriverWait(driver, WAIT).until(EC.presence_of_element_located((By.ID, 'kc-login')))
 username = driver.find_element(By.ID, 'username')
 password = driver.find_element(By.ID, 'password')
-username.send_keys(user)
+username.send_keys(USER)
 password.send_keys(keyring.get_password('auth.metadatacenter.org', 'pschumm@uchicago.edu'))
 driver.find_element(By.ID, 'kc-login').click()
+logger.info(f'Logged in as {USER}')
 
-# Populate template
-WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.ID, template)))
-driver.find_element(By.XPATH, f'//div[@id="{template}"]//button').click()
-
-# TODO Add remaining fields, including fixed choice
-enter_text('Study Title or Name', 'My Awesome Study')
-enter_text('Study Description or Abstract', 'A study to supercede all others, seriously')
-# BEGIN ADDED by TMH 05/23/2022
-enter_text('Name of the study group or collection(s) to which this study belongs', 'N/A')
-enter_text('Funder or Grant Agency Name', 'Grant Funder')
-enter_text('Funder or Grant Agency Abbreviation or Acronym', 'GF')
-enter_text('Funding or Grant Award ID', 'awdid555')
-enter_text('Funding or Grant Award Name', 'awdname')
-enter_text('Investigator First Name', 'Testy')
-enter_text('Investigator Middle Initial', 'T')
-enter_text('Investigator Last Name', 'Tester')
-enter_text('Investigator Institutional Affiliation', 'School of Hard Knocks')
-enter_text('Identifier Value', "MyID")
-enter_text('Contact First Name', 'Contact')
-enter_text('Contact Middle Initial', 'Me')
-enter_text('Contact Last Name', 'Please')
-enter_text('Contact Affiliation', 'Gofer')
-enter_text('Contact Email', 'Gofer@gofer.org')
-enter_text('Registrant First Name', 'Reg')
-enter_text('Registrant Middle Initial', 'E')
-enter_text('Registrant Last Name', 'Ster')
-enter_text('Registrant Affiliation', 'Just a friend')
-enter_text('Registrant Email', 'reg@ster.org')
-enter_text('Date when first data will be collected/produced (Anticipated)', '01/01/2022')
-enter_text('Date when last data will be collected/produced (Anticipated)', '12/31/2025')
-enter_text('Date when first data will be released (Anticipated)', '01/01/2023')
-enter_text('Date when last data will be released (Anticipated)', '04/01/2026')
-enter_text('Primary Publications DOI', 'DOIxxxxx')
-enter_text('Primary Study Findings', 'Found some good stuff')
-enter_text('Secondary Publications DOI', '2DOIxxxxx')
-enter_text('Study Nickname or Alternative Title', 'Seriously supercede')
-enter_text('NIH Application ID', '123456789')
-enter_text('NIH RePORTER Link, 'https://healdata.org)
-enter_text('ClinicalTrials.gov Study ID', 'NCT123456789')
-enter_text('CEDAR Study-level Metadata Template Instance ID', '12345678-1234-abcd-5678-87654321')
-enter_text('Other Study-Associated Websites', 'https://healdata.org')
-enter_text('Name of Repository', 'JimBobs Data Wharehouse')
-enter_text('Study ID assigned by Repository', 'abc123')
-enter_text('Repository-branded Study Persistent Identifier', 'xyz789')
-enter_text('Study citation at Repository', 'something something citation')
-# END ADDED by TMH 05/23/2022
-
-# Save and return to main list
-driver.find_element(By.XPATH, '//button[text()="Save"]').click()
-uuid = WebDriverWait(driver, wait).until(wait_for_uuid())
-with open('metadata-instances.txt', 'a') as f:
-    f.write(uuid + '\n')
-WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Created")]')))
-driver.find_element(By.XPATH, '//i[contains(@class, "back-arrow")]').click()
+while True:
+    uuid, values = populate_template(TEMPLATE, form)
+    verify_metadata(uuid, values)
+    delete_instance(uuid)
+    time.sleep(10)
 
 driver.quit()
