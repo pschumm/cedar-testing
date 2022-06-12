8a21b7720460eaa57eef4afeab75d0a1d63b757e
Phil Schumm <pschumm@uchicago.edu>
2022-06-12 16:36:55 -0500

    Fix script to print instance

--- print_instance.py
+++ print_instance.py
@@ -1,16 +1,16 @@
-# Print instance of CEDAR metadata
+# Print instance of CEDAR metadata in JSON-LD format
 import requests
 import keyring
 import json
 
-UUID = 
+UUID = ''
 SITE = 'https://resource.metadatacenter.org'
 ENDPOINT='template-instances'
 KEY = keyring.get_password('resource.metadatacenter.org', 'pschumm@uchicago.edu')
 
-print(f'ID: {uuid.rstrip()}\n')
-instance = f'https%3A%2F%2Frepo.metadatacenter.org%2Ftemplate-instances%2F{uuid.rstrip()}'
-url = f'{SITE}/{ENDPOINT}/{instance}?format=json'
+print(f'UUID: {UUID}\n')
+instance = f'https%3A%2F%2Frepo.metadatacenter.org%2Ftemplate-instances%2F{UUID}'
+url = f'{SITE}/{ENDPOINT}/{instance}?format=jsonld'
 response = requests.get(url, headers={'Accept':'application/json',
                                       'Authorization':f'apiKey {KEY}'})
 print(json.dumps(response.json(), indent=4), '\n')
